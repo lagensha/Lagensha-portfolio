3c5453b0ee0e407a77c850e67a38f29f128cf656
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-04-19 13:48:51 +0530

    feat: update project entries with new titles and detailed summaries for ThogaKade and BurgerShop

--- src/app/portfolio/portfolio.ts
+++ src/app/portfolio/portfolio.ts
@@ -31,10 +31,16 @@ export class Portfolio {
         'Modern Grocery Shop web application developed to deliver a smooth and efficient online shopping experience. Built with a focus on clean design, user-friendly navigation, and real-world eCommerce functionality. Features include product browsing, category management, shopping cart operations, secure checkout flow, and responsive UI for all devices. Designed to demonstrate full-stack development skills, problem-solving ability, and practical implementation of scalable retail solutions. Ideal for modern digital grocery businesses.',
     },
     {
-      title: 'Service Booking Platform',
-      stack: 'Angular, Spring Security, SQL',
+      title: 'ThogaKade-Layered Architecture',
+      stack: 'javaFx,Layered Architecture,SQL',
       summary:
-        'A customer-facing booking experience with secure authentication and a mobile-first UX architecture.',
+        'This is third step of thoga kade project i created using javafx . previous project was developed by using ObservableList , database and Singleton Design Pattern but when we get real world project create using layered Architecture not MVC .',
+    },
+    {
+      title: 'BurgerShop-Web-Based-Point-of-Sale-POS-System',
+      stack: 'HTML, CSS, JavaScript',
+      summary:
+        'A responsive web-based Point of Sale system designed for fast-food and burger shop operations. This project includes order management, item handling, billing, customer interaction, and real-time UI updates using HTML, CSS, and JavaScript. The system is optimized for ease of use, quick navigation, and efficient restaurant workflow.',
     },
   ];
 
